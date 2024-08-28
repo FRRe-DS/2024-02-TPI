255ef6ab9fa3b6f24838fd241f7b4df3bcb1a43d
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2024-08-28 20:39:22 -0300

    Estuve viendo un poco las guias de DRF y escribi algunos ejemplos para practicar.

--- server/src/app/views.py
+++ server/src/app/views.py
@@ -1,14 +1,30 @@
-from django.http import HttpResponse, HttpRequest
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from app.models import UserCustom
+from app.serializers import UserSerializer
+from rest_framework import status
 
 # Create your views here.
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Indice")
+
+@api_view(["GET"])
+def health_check(request: Request) -> Response:
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-def saludo(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hola! Aca vamos a hacer el tp!")
+@api_view(["GET"])
+def getUserData(request: Request) -> Response:
+    users = UserCustom.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(status=status.HTTP_200_OK, data=serializer.data)
 
 
-def health_check(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(status=204)
+@api_view(["POST"])
+def addUser(request: Request) -> Response:
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.data)
