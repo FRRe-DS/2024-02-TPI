94eaa121e012c3be0979c8211d41404638458e8f
enzo vallejos <enzonahuelvallejos@gmail.com>
2024-08-31 12:55:55 -0300

    agregando las clases

--- server/src/app/models.py
+++ server/src/app/models.py
@@ -28,4 +28,32 @@ class UserCustom(models.Model):
     username = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
+#falta el qr pero no se me ocurre como modelarlo
+class Escultura(models.Model):
+    ID_escultura = models.BigAutoField(primary_key=True) 
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+    f_creacion = models.DateField() 
 
+#falta foto de perfil pero ahora vemos eso
+class Escultor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=20)
+    correo = models.CharField(max_length=100, primary_key=True)
+    bibliografia = models.CharField(max_length=400)
+
+class Visitante(models.Model):
+    correo = models.CharField(max_length=100, primary_key=True)
+    ID_escultura = models.BigAutoField() 
+
+class Adm_sistemas(models.Model):
+    correo = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=32)
+
+#falta el campo imagen
+class Imagenes(models.Model):
+    fecha = models.DateField() 
+
+class Evento(models.Model):
+
+class Tematica(models.Model):
