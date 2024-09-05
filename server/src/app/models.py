124efbf31840fdc2893815a90b969b3a79006996
Saucedo-Gonzalo <gonzalosaucedoo15@gmail.com>
2024-09-05 10:00:37 -0300

    agregando/modif modelos

--- server/src/app/models.py
+++ server/src/app/models.py
@@ -1,61 +1,97 @@
 # Create your models here.
 
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+# from pygments.lexers import get_all_lexers
+# from pygments.styles import get_all_styles
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
-
-
-class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default="")
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(
-        choices=LANGUAGE_CHOICES, default="python", max_length=100
-    )
-    style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
-
-    class Meta:
-        ordering = ["created"]
+# LEXERS = [item for item in get_all_lexers() if item[1]]
+# LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+# STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
-class UserCustom(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    username = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+# class Snippet(models.Model):
+#     created = models.DateTimeField(auto_now_add=True)
+#     title = models.CharField(max_length=100, blank=True, default="")
+#     code = models.TextField()
+#     linenos = models.BooleanField(default=False)
+#     language = models.CharField(
+#         choices=LANGUAGE_CHOICES, default="python", max_length=100
+#     )
+#     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+
+#     class Meta:
+#         ordering = ["created"]
+
+
+# class UserCustom(models.Model):
+#     id = models.BigAutoField(primary_key=True)
+#     username = models.CharField(max_length=200)
+#     created = models.DateTimeField(auto_now_add=True)
 
 #falta el qr pero no se me ocurre como modelarlo
 class Escultura(models.Model):
-    ID_escultura = models.BigAutoField(primary_key=True) 
-    nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=300)
-    f_creacion = models.DateField() 
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
+    fecha_creacion = models.DateField() 
 
-#falta foto de perfil pero ahora vemos eso
+#ready
+class Pais(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+
+#ready
 class Escultor(models.Model):
-    nombre = models.CharField(max_length=100)
-    nacionalidad = models.CharField(max_length=20)
-    correo = models.CharField(max_length=100, primary_key=True)
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    pais_id = models.ForeignKey(Pais, on_delete=models.CASCADE, db_column='pais_id')
+    correo = models.EmailField(null=True, blank=True, unique=True)
+    foto = models.FileField(upload_to='perfiles/', blank=True, null=True)
     bibliografia = models.CharField(max_length=400)
 
+#ready
 class Visitante(models.Model):
-    correo = models.CharField(max_length=100, primary_key=True)
-    ID_escultura = models.BigAutoField() 
+    id = models.AutoField(primary_key=True)
+    correo = models.EmailField(null=True, blank=True, unique=True)
 
+#ready
 class Adm_sistemas(models.Model):
-    correo = models.CharField(max_length=100, primary_key=True)
+    id = models.AutoField(primary_key=True)
+    correo = models.EmailField(null=True, blank=True, unique=True)
     password = models.CharField(max_length=32)
 
-#falta el campo imagen
+#ready
 class Imagenes(models.Model):
+    id = models.AutoField(primary_key=True)
     fecha = models.DateField() 
+    imagen = models.FileField(upload_to='imagenes/', blank=True, null=True)
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
 
+
+#ready
+class Tematica(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
+
+#ready
+class Lugar(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
+
+#ready
 class Evento(models.Model):
-    nombre = models.CharField(max_length=100, primary_key=True)
-    lugar = models.CharField(max_length=100)
-    fecha = models.DateField() 
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False)
+    lugar_id = models.ForeignKey(Lugar, on_delete=models.CASCADE, db_column='lugar_id')
+    fecha_inicio = models.DateField() 
+    fecha_fin = models.DateField() 
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
+    tematica_id = models.ForeignKey(Tematica, on_delete=models.CASCADE, db_column='tematica_id')
+    
+
+
+
+
 
