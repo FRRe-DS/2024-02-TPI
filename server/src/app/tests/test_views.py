6b06a8962131bcf8599cff9b97ec0cfa443673bf
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2024-09-06 17:31:02 -0300

    Lista de cambios: 1. Corregí en app/models.py, en el modelo `Visitante` que los correos puedan ser nulos o puedan ser el string vacío, me parece que para el escenario es necesario que el email esté presente.
    2. Añadí casos de pruebas para los endpoints `addVisitante` y `getVisitantesData`, podría plantearse
    un tercer endpoint para ver los datos de un Visitante en particular pero tendría que medirse qué tan útil sería.
    
    3. Añadí casos de pruebas unitarias para ambos endpoints, tanto para `getVisitantesData` como
    para `addVisitante`.

--- server/src/app/tests/test_views.py
+++ server/src/app/tests/test_views.py
@@ -1,47 +1,63 @@
 from django.test import SimpleTestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APITestCase
 
-from app.models import UserCustom
-from app.serializers import UserSerializer
+from app.models import Visitante
+from app.serializers import VisitanteSerializer
 
 
-class SimpleTestVistas(SimpleTestCase):
+class HealthCheckAPITest(SimpleTestCase):
     def test_health_check(self):
         response = self.client.get(reverse("health_check"))
         self.assertEqual(response.status_code, 204)
 
 
-class UserCustomViewsTestCase(APITestCase):
+class VisitanteAPITest(APITestCase):
     def setUp(self):
-        self.user = UserCustom.objects.create(username="testuser")
-        self.get_url = reverse("getUserData")
-        self.post_url = reverse("addUser")
+        self.client = APIClient()
+        self.get_url = reverse("getVisitantesData")
+        self.post_url = reverse("addVisitante")
 
-    def test_get_user_data_200_OK(self):
+        Visitante.objects.create(correo="acostalautaro@ejemplo.com")
+        Visitante.objects.create(correo="gonza_saucedo@ejemplo.com")
+
+    def test_get_visitante_data_200_OK(self):
         response = self.client.get(self.get_url)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
 
-        expected_data = UserSerializer(UserCustom.objects.all(), many=True).data
+        expected_data = VisitanteSerializer(Visitante.objects.all(), many=True).data
         self.assertEqual(expected_data, response.data)
 
     def test_add_user_201_CREATED(self):
-        data = {
-            "username": "nuevo",
-        }
+        valid_emails = {"correo": "Xxenzo_vallejosxX@xbox.com"}
 
-        response = self.client.post(self.post_url, data, format="json")
+        response = self.client.post(self.post_url, valid_emails, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["username"], "nuevo")
-        self.assertTrue(UserCustom.objects.filter(username="nuevo").exists())
+        self.assertEqual(response.data["correo"], "Xxenzo_vallejosxX@xbox.com")
+        self.assertTrue(
+            Visitante.objects.filter(correo="Xxenzo_vallejosxX@xbox.com").exists()
+        )
 
-    def test_add_user_empty_string_400_BAD_REQUEST(self):
-        data = {
-            "username": "",
-        }
+    def test_add_visitante_400_BAD_REQUEST(self):
+        # La clase `EmailValidator` de Django ya se adhiere a los estándares de emails RFC 5321 y RFC 5322,
+        # y además nuestro escenario no tiene ninguna restricción especial en tanto a los correos que debe aceptar
+        # por lo que me parece que, de momento, estos controles son suficientes para nuestra aplicación y no haría
+        # falta suar librerías como hypothesis para controlar las posibles entradas.
 
-        response = self.client.post(self.post_url, data, format="json")
+        invalid_emails = [
+            "",
+            1,
+            "lautaro",
+            "@",
+            "lautaro@",
+            "@ejemplo",
+            "@ejemplo.com",
+            "@.com",
+        ]
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        for email in invalid_emails:
+            data = {"correo": email}
+            response = self.client.post(self.post_url, data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
