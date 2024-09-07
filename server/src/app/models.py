f0954eac03af381b98afa2f34977e7f6d4b70d4e
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2024-09-07 18:01:34 -0300

    Añadi algunos comentarios con respecto a los modelos

--- server/src/app/models.py
+++ server/src/app/models.py
@@ -1,57 +1,52 @@
 from django.db import models
 
+class Visitante(models.Model):
+    id = models.AutoField(primary_key=True)
+    correo = models.EmailField(null=False, blank=False, unique=True)
 
 class Escultura(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
     descripcion = models.CharField(max_length=300, blank=True, null=True)
     fecha_creacion = models.DateField()
+    # Tiene sentido almacenar los códigos QR si van a ser regenerados cada 10 minutos?
     qr = models.FileField(upload_to="qr/", blank=True, null=True)
-
 
 class Pais(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
-
 
 class Escultor(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
     pais_id = models.ForeignKey(Pais, on_delete=models.CASCADE, db_column="pais_id")
     correo = models.EmailField(null=True, blank=True, unique=True)
+    # TODO: (Lautaro) Si quisieramos trabajar usando un Object Storage como S3 o R2 para guardar las imágenes, este campo tendría que ser un URLField.
     foto = models.FileField(upload_to="perfiles/", blank=True, null=True)
     bibliografia = models.CharField(max_length=400)
-
-
-class Visitante(models.Model):
-    id = models.AutoField(primary_key=True)
-    correo = models.EmailField(null=False, blank=False, unique=True)
-
 
 class Adm_sistemas(models.Model):
     id = models.AutoField(primary_key=True)
     correo = models.EmailField(null=True, blank=True, unique=True)
     password = models.CharField(max_length=32)
 
-
 class Imagen(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField()
+    # TODO: (Lautaro) Si quisieramos trabajar usando un Object Storage como S3 o R2 para guardar las imágenes, este campo tendría que ser un URLField.
+    # imagen = models.URLField(max_length=200)
     imagen = models.FileField(upload_to="imagenes/", blank=True, null=True)
     descripcion = models.CharField(max_length=255, blank=True, null=True)
-
 
 class Tematica(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
     descripcion = models.CharField(max_length=255, blank=True, null=True)
 
-
 class Lugar(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
     descripcion = models.CharField(max_length=255, blank=True, null=True)
-
 
 class Evento(models.Model):
     id = models.AutoField(primary_key=True)
@@ -63,7 +58,6 @@ class Evento(models.Model):
     tematica_id = models.ForeignKey(
         Tematica, on_delete=models.CASCADE, db_column="tematica_id"
     )
-
 
 class EsculturaImagen(models.Model):
     id = models.AutoField(primary_key=True)
