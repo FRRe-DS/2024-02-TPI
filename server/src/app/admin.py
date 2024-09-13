6df852f4dbc58ff16d088fd7180fea3e9757238f
Ivan Niveyro <iiniveyro@gmail.com>
2024-09-13 17:12:04 -0300

    Se crearon las vistas para poder hacer CRUD en la BD usando ViewSets provistas por Rest Framework

--- server/src/app/admin.py
+++ server/src/app/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Escultura)
+admin.site.register(Escultor)
+admin.site.register(Evento)
+admin.site.register(Imagen)
+admin.site.register(Visitante)
--- server/src/app/urls.py
+++ server/src/app/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
-from . import views
+from .views import *
+#from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+
+router.register('api/visitantes', VisitanteViewSet, 'visitantes')
+router.register('api/escultores', EscultorViewSet, 'escultores')
+router.register('api/esculturas', EsculturaViewSet, 'esculturas')
+router.register('api/imagenes', ImagenViewSet, 'imagenes')
+router.register('api/pais', PaisViewSet, 'paises')
+router.register('api/adminsis', AdminSisViewSet, 'adminsis')
+router.register('api/tematica', TematicaViewSet, 'tematicas')
+router.register('api/lugar', LugarViewSet, 'lugares')
+
+urlpatterns = router.urls
+
+'''
 urlpatterns = [
+    router.urls,
     path("", views.getVisitantesData, name="getVisitantesData"),
     path("add/", views.addVisitante, name="addVisitante"),
-    path("health_check", views.health_check, name="health_check"),
+    path("health_check/", views.health_check, name="health_check"),
+    path("getescultor/", views.getEscultor, name="getEscultor"),
+    path("getesculturas/", views.getEsculturas, name="getEscultoras"),
+    path("geteventos/", views.getEventos, name="getEventos"),
+    path("getimagenes/", views.getImg, name="getImg")
 ]
+'''
