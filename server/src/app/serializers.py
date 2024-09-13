304647ab17ff5e1f3d5534fef12f6b576c0c07fc
Ivan Niveyro <iiniveyro@gmail.com>
2024-09-13 17:12:04 -0300

    Se crearon las vistas para poder hacer CRUD en la BD usando ViewSets provistas por Rest Framework

--- server/src/app/serializers.py
+++ server/src/app/serializers.py
@@ -1,8 +1,47 @@
 from rest_framework import serializers
-from app.models import Visitante
-
+from .models import *
 
 class VisitanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visitante
+        fields = "__all__"
+
+class EventoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evento
+        fields = "__all__"
+
+class EsculturaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Escultura
+        fields = "__all__"
+
+class EscultorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Escultor
+        fields = "__all__"
+
+class ImagenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Imagen
+        fields = "__all__"
+
+class PaisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pais
+        fields = "__all__"
+
+class AdminSisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Adm_sistemas
+        fields = "__all__"
+
+class TematicaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tematica
+        fields = "__all__"
+
+class LugarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lugar
         fields = "__all__"
