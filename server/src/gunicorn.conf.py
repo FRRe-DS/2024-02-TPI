b6f77cca67aa72518632bee55c17760899f8b4c8
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2024-09-20 17:09:04 -0300

    Pequeñas correcciones

--- server/src/gunicorn.conf.py
+++ server/src/gunicorn.conf.py
@@ -1,4 +1,5 @@
 # https://docs.gunicorn.org/en/stable/settings.html#settings
+from gunicorn import glogging
 
 bind = "0.0.0.0:8000"
 # workers = multiprocessing.cpu_count() * 2 + 1
@@ -17,19 +18,17 @@ loglevel = "info"
 limit_request_line = 4094
 
 
-# class JSONLogger(glogging.Logger):
-#     def setup(self, cfg):
-#         super().setup(cfg)
+class JSONLogger(glogging.Logger):
+    def setup(self, cfg):
+        super().setup(cfg)
 
-#         json_formatter = jsonlogger.JsonFormatter(
-#             "%(asctime)s %(levelname)s %(name)s %(message)s %(filename)s"
-#         )
-#         for handler in self.access_log.handlers:
-#             handler.setFormatter(json_formatter)
+        json_formatter = jsonlogger.JsonFormatter(
+            "%(asctime)s %(levelname)s %(name)s %(message)s %(filename)s"
+        )
+        for handler in self.access_log.handlers:
+            handler.setFormatter(json_formatter)
 
-#         for handler in self.error_log.handlers:
-#             handler.setFormatter(json_formatter)
+        for handler in self.error_log.handlers:
+            handler.setFormatter(json_formatter)
 
-
-JSON_LOGS = True
-# logger_class = JSONLogger
+logger_class = JSONLogger
