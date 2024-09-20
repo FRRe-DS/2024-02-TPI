62ddf72a5486fe0a05db69212a41239583390562
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2024-09-20 14:08:56 -0300

    Añado los archivos que hace falta para construir el contenedor

---
+++ server/src/gunicorn.conf.py
@@ -0,0 +1,46 @@
+# https://docs.gunicorn.org/en/stable/settings.html#settings
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+accesslog = "-"
+errorlog = "-"
+timeout = 30
+daemon = False
+
+# https://docs.gunicorn.org/en/stable/settings.html#access-log-format
+# t: fecha, h: dirección remota, r: status line, s: status, b: largo de la respuesta
+# L: tiempo de respuesta en segundos, f: referrer, a: user agent
+access_log_format = """
+    {
+        "timestamp": "%(t)s",
+        "remote_addr": "%(h)s",
+        "request": "%(r)s",
+        "status": "%(s)s",
+        "bytes": "%(b)s",
+        "latency": "%(L)s",
+        "referer": "%(f)s",
+        "user_agent": "%(a)s"
+    } 
+"""
+loglevel = "info"
+
+limit_request_line = 4094
+
+
+# class JSONLogger(glogging.Logger):
+#     def setup(self, cfg):
+#         super().setup(cfg)
+
+#         json_formatter = jsonlogger.JsonFormatter(
+#             "%(asctime)s %(levelname)s %(name)s %(message)s %(filename)s"
+#         )
+#         for handler in self.access_log.handlers:
+#             handler.setFormatter(json_formatter)
+
+#         for handler in self.error_log.handlers:
+#             handler.setFormatter(json_formatter)
+
+
+JSON_LOGS = True
+# logger_class = JSONLogger
