ec78a6e16c3ddbf5e3d5dc8321a3a6b68d172b26
Tobias <tobiassebastianstegmayer@gmail.com>
2024-11-17 21:27:27 +0000

    Finalice la pantalla de detalle_evento

--- landing/src/js/detalle_evento.ts
+++ landing/src/js/detalle_evento.ts
@@ -4,7 +4,7 @@ import { formatearFecha, loadHTML } from "../app";
 
 const URL_EVENTOS = "http://localhost:8000/api/eventos/";
 const URL_LUGAR= "http://localhost:8000/api/lugar/";
-const URL_MATERIAL= "http://localhost:8000/api/tematica/";
+
 
 async function loadLugar(URL:string, id:string){
   try{
@@ -20,45 +20,32 @@ async function loadLugar(URL:string, id:string){
 }
 
 
-async function loadTematica(URL:string, id:string){
-  try{
-    const res = await fetch(`${URL}${id}`)
-    const tematica = await res.json() 
-    console.log(tematica)
-    return tematica
-   
-  }catch(error){
-    console.log(`Error al carga la tematica: ${error}`);
-  }
-  
-}
-
 async function loadEvento(URL:string, id:string) {
   try{
     const res = await fetch(`${URL}${id}`)
     const evento = await res.json() 
 
     const titulo = document.querySelector("#nombre-evento") as HTMLHeadingElement
+    const titulo2 = document.querySelector("#nombre-evento-h2") as HTMLHeadingElement
     const fecha = document.querySelector("#fecha-evento") as HTMLHeadingElement
     const descripcion = document.querySelector("#descripcion-evento") as HTMLParagraphElement
-    const lugarNombre = document.querySelector("#lugar-evento") as HTMLSpanElement
-    const lugarDescripcion = document.querySelector("#lugar-descripcion") as HTMLSpanElement
+    const lugarNombre = document.querySelector("#lugar-evento") as HTMLHeadingElement
+    const lugarDescripcion = document.querySelector("#lugar-descripcion") as HTMLParagraphElement
     const lugar = await loadLugar(URL_LUGAR, evento.lugar_id) 
-    const tematicaNombre = document.querySelector("#evento-tematica") as HTMLSpanElement
-    const tematicaDescripcion = document.querySelector("#evento-descripcion") as HTMLSpanElement
-   
-    const tematica = await loadTematica(URL_MATERIAL, evento.tematica_id) 
-
+ 
+    const imagenEvento = document.querySelector("#imagen-evento") as HTMLImageElement 
     
     titulo.textContent = evento.nombre
+    titulo2.textContent = evento.nombre
     fecha.textContent = `${formatearFecha(evento.fecha_inicio)} - ${formatearFecha(evento.fecha_fin)}`
     descripcion.textContent = evento.descripcion
     lugarNombre.textContent = lugar.nombre
     lugarDescripcion.textContent = lugar.descripcion
-    tematicaNombre.textContent = tematica.nombre
-    tematicaDescripcion.textContent = tematica.descripcion
 
-    console.log(evento)
+    imagenEvento.src = evento.foto;
+    imagenEvento.title = evento.nombre;
+
+ 
   }catch(error){
     console.log(`Error al carga el evento: ${error}`);
   }
--- landing/src/js/eventos.ts
+++ landing/src/js/eventos.ts
@@ -1,35 +1,6 @@
 const URL_EVENTOS = "http://localhost:8000/api/eventos/";
 import { formatearFecha, loadHTML } from "../app";
 
-
-
-// ------ TODO: Elegir imagen, esto es solamente provisorio ahsta que gonza agregue las imagenes en la BD ------
-const imagenes = [
-	"../images/Eventos/ConcursoInternacionalDeEsculturas.jpg",
-	"../images/Eventos/ConferenciasYcharlas.jpg",
-	"../images/Eventos/EspectáculosMusicaEnVivo.jpg",
-	"../images/Eventos/ExposicionesDeArte.jpg",
-	"../images/Eventos/IntervencionesUrbanas.jpg",
-	"../images/Eventos/RecorridosGuiado.jpg",
-	"../images/Eventos/TalleresYCapacitacion.jpg",
-	"../images/Eventos/ExhibicionesDeRealidadVirtual.jpg",
-	"../images/Eventos/PerformanceEnVivo.jpg",
-	"../images/Eventos/FestivalGastronomico.jpg",
-	"../images/Eventos/ProyeccionesDeCine.jpg",
-	"../images/Eventos/PremiaciónyClausura.jpg",
-	"../images/Eventos/ActividadesParaNiños.jpg",
-	"../images/Eventos/ArtistasInvitadosInternacionales.jpg",
-	"../images/Eventos/FeriaDeArtesania.jpg",
-];
-
-export function elegirImagen(nombreImg: string) {
-	const indice = imagenes.findIndex((ruta) =>
-		ruta.includes(nombreImg.split(" ")[0]),
-	);
-
-	return imagenes[indice];
-}
-
 // ------ Get eventos ------
 
 async function loadEventos(url: string) {
@@ -45,10 +16,8 @@ async function loadEventos(url: string) {
 				const card = document.createElement("div");
 				card.classList.add("event-card");
 
-				// card.style.backgroundImage = `url('${elegirImagen(evento.nombre)}')`
-
 				card.innerHTML = `
-            <img class="card-img-evento" loading="lazy" src="${elegirImagen(evento.nombre)}" alt="${evento.nombre}">
+            <img class="card-img-evento" loading="lazy" src="${evento.foto}" alt="${evento.nombre}">
             <div class="card-content">
               <h2>${evento.nombre}</h2>
               <div>
