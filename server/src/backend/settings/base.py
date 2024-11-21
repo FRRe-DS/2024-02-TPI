b6ab71071db385491a55e86ba0b9c042f5d9fac7
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-21 19:27:12 -0300

    .

--- server/src/backend/settings/base.py
+++ server/src/backend/settings/base.py
@@ -99,9 +99,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
+    "https://elrincondelinge.org",
 ]
 
 ROOT_URLCONF = "backend.urls"
