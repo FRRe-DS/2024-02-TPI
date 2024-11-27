2809272932f98441cba39da0ae3005597831373d
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-27 01:24:06 -0300

    Ya me acorde de por qué hacía falta testing jaja.

---
+++ server/src/backend/settings/testing.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+SECRET_KEY = "django-insecure-j+&k(*o_vgi3d01+n^#r14+dagby)7-&-iq!@_2$2t(hd6hw7)"
+
+DEBUG = True
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+ALLOWED_HOSTS = "localhost,127.0.0.1".split(",")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tpi",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "OPTIONS": {
+            "pool": True,
+        },
+    },
+}
