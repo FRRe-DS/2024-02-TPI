82a9b8ef9281e27fef8f5a303f07e9964b3629a3
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-29 15:56:44 -0300

    .

--- landing/src/dev-dist/sw.js
+++ landing/src/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4j76b3nrg3o"
+    "revision": "0.73b3mmtm5ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
