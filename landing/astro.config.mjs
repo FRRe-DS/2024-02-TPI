b740d05b0734560f51bdfb6d986265dda04ea73c
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-12-03 19:26:40 -0300

    Correciones en las imagenes que son cargadas en index.astro

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -6,6 +6,10 @@ import netlify from "@astrojs/netlify";
 
 import AstroPWA from "@vite-pwa/astro";
 
+import compressor from "astro-compressor";
+
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -54,6 +58,8 @@ export default defineConfig({
 				directoryAndTrailingSlashHandler: true,
 			},
 		}),
+		purgecss(),
+		compressor(),
 	],
 
 	experimental: {
@@ -73,3 +79,4 @@ export default defineConfig({
 	output: "hybrid",
 	adapter: netlify(),
 });
+
